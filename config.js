96fc6a8490058b4cfb9410ea5057f4fb482c824f
Kyle Melton <kalm42@gmail.com>
2019-06-07 12:52:31 -0500

    fix: Remove production endpoint

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const endpoint = `http://localhost:4444`;
-export const productionEndpoint = `https://eps-backend.now.sh`;
+export const productionEndpoint = ``;
 export const perPage = 4;
 export const backendUrl = process.env.NODE_ENV === 'development' ? endpoint : productionEndpoint;
