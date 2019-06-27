cb730209a169a764f46388e50977e70595367848
Kyle Melton <kalm42@gmail.com>
2019-06-27 12:14:50 -0800

    refactor: Move single user query to shared queries

--- src/shared/queries.js
+++ src/shared/queries.js
@@ -11,3 +11,21 @@ export const CURRENT_USER_QUERY = gql`
     }
   }
 `;
+
+export const USER_QUERY = gql`
+  query USER_QUERY($id: ID!) {
+    user(where: { id: $id }) {
+      id
+      name
+      email
+      guestCount
+      address {
+        line1
+        line2
+        city
+        state
+        zip
+      }
+    }
+  }
+`;
