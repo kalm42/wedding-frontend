243363ace8ece0616da9f71922c6906856958f68
Kyle Melton <kalm42@gmail.com>
2019-07-18 15:30:39 -0600

    feat: Connect the send invite button to the api call on the backend

--- src/pages/guest-management/list-guests.js
+++ src/pages/guest-management/list-guests.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Query } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 import styled from 'styled-components';
 import { Link } from 'gatsby';
+import gql from 'graphql-tag';
 
 import Layout from '../../components/layout';
 import SEO from '../../components/seo';
 import ErrorMessage from '../../components/ErrorMessage';
-import { LinkButton, TwoColumns } from '../../shared/styledComponents';
+import { LinkButton, TwoColumns, Success } from '../../shared/styledComponents';
 import { GUESTS_QUERY } from '../../shared/queries';
 import DeleteGuest from '../../components/DeleteGuest';
 import PleaseSignIn from '../../components/PleaseSignIn';
@@ -20,13 +21,39 @@ const EditButton = styled(Link)`
   box-shadow: var(--deepboxshadow);
 `;
 
+const CREATE_INVITATIONS_MUTATION = gql`
+  mutation CREATE_INVITATIONS_MUTATION {
+    createInvitations {
+      id
+    }
+  }
+`;
+
 const ListGuests = () => (
   <Layout>
     <SEO title="Guest Management" keywords={[`gatsby`, `application`, `react`]} />
     <h1>All Guests</h1>
     <TwoColumns>
       <LinkButton to="/guest-management/invite">Invite another guest</LinkButton>
-      <button type="button">Send invites</button>
+      <Mutation mutation={CREATE_INVITATIONS_MUTATION}>
+        {(createInvitations, { error, loading, called }) => {
+          if (error) {
+            return <ErrorMessage error={error} />;
+          }
+          if (called) {
+            return (
+              <Success>
+                <p>Invites sent!</p>
+              </Success>
+            );
+          }
+          return (
+            <button type="button" disabled={loading} onClick={() => createInvitations()}>
+              Send{loading && 'ing'} invites
+            </button>
+          );
+        }}
+      </Mutation>
     </TwoColumns>
     <PleaseSignIn admin>
       <Query query={GUESTS_QUERY}>
