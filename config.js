cbae336880350fd08c0b583744bec6bffdb79983
Kyle Melton <kalm42@gmail.com>
2019-07-19 13:55:33 -0600

    fix: Assign production backend variable

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const endpoint = process.env.GATSBY_BACKEND_URL;
-export const productionEndpoint = ``;
+export const productionEndpoint = `https://wedding-backend.now.sh`;
 export const perPage = 4;
 export const backendUrl = process.env.NODE_ENV === 'development' ? endpoint : productionEndpoint;
 export const SENTRY_DSN = process.env.GATSBY_SENTRY_DSN;
