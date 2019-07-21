033a813a211ccfe192663c99ca38d70c9ee0171d
Kyle Melton <kalm42@gmail.com>
2019-07-21 14:39:54 -0600

    fix: Remove duplicate query

--- src/pages/history.js
+++ src/pages/history.js
@@ -6,7 +6,6 @@ import { format } from 'date-fns';
 import SEO from '../components/seo';
 import Layout from '../components/layout';
 import ErrorMessage from '../components/ErrorMessage';
-import User from '../components/User';
 import { USER_TRANSACTION_LIST_QUERY } from '../shared/queries';
 import PleaseSignIn from '../components/PleaseSignIn';
 import GiftGraph from '../components/GiftGraph';
@@ -25,60 +24,48 @@ const history = () => {
         <GiftGraph />
         <h2>Transactions</h2>
         <PleaseSignIn>
-          <User>
-            {({ data: { me }, loading, error }) => {
+          <Query query={USER_TRANSACTION_LIST_QUERY}>
+            {({ data, loading, error, refetch }) => {
               if (loading) {
                 return <Loading />;
               }
               if (error) {
                 return <ErrorMessage error={error} />;
               }
+              const { transactions } = data;
               return (
-                <Query query={USER_TRANSACTION_LIST_QUERY} variables={{ userId: me.id }}>
-                  {({ data: { transactions }, loading, error, refetch }) => {
-                    if (loading) {
-                      return <Loading />;
-                    }
-                    if (error) {
-                      return <ErrorMessage error={error} />;
-                    }
-                    return (
-                      <>
-                        <table>
-                          <thead>
-                            <tr>
-                              <th>Date</th>
-                              <th>Choice</th>
-                              <th>Amount</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            {transactions.map(transaction => {
-                              const date = new Date(transaction.createdAt);
-                              const choice =
-                                transaction.gift === 'GYM' ? 'Gym' : 'Italian Honeymoon';
-                              return (
-                                <tr key={transaction.id}>
-                                  <td>
-                                    {format(date, 'MM/DD/YYYY')} at {format(date, 'h:mm:ss')}
-                                  </td>
-                                  <td>{choice}</td>
-                                  <td>${transaction.price / 100}</td>
-                                </tr>
-                              );
-                            })}
-                          </tbody>
-                        </table>
-                        <button type="button" onClick={() => refetch()}>
-                          Refresh
-                        </button>
-                      </>
-                    );
-                  }}
-                </Query>
+                <>
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>Date</th>
+                        <th>Choice</th>
+                        <th>Amount</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {transactions.map(transaction => {
+                        const date = new Date(transaction.createdAt);
+                        const choice = transaction.gift === 'GYM' ? 'Gym' : 'Italian Honeymoon';
+                        return (
+                          <tr key={transaction.id}>
+                            <td>
+                              {format(date, 'MM/DD/YYYY')} at {format(date, 'h:mm:ss')}
+                            </td>
+                            <td>{choice}</td>
+                            <td>${transaction.price / 100}</td>
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </table>
+                  <button type="button" onClick={() => refetch()}>
+                    Refresh
+                  </button>
+                </>
               );
             }}
-          </User>
+          </Query>
         </PleaseSignIn>
       </div>
     </Layout>
