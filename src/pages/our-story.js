ab07e2c149bb02d5af737a8eea0da11d156747c9
Kyle Melton <kalm42@gmail.com>
2019-09-21 16:57:48 -0600

    feat: Add pages

---
+++ src/pages/our-story.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import SEO from '../components/seo';
+import Layout from '../components/layout';
+import { TwoColumns } from '../shared/styledComponents';
+import KSImage000 from '../components/images/KSImage000';
+import KSImage001 from '../components/images/KSImage001';
+import KSImage002 from '../components/images/KSImage002';
+import KSImage003 from '../components/images/KSImage003';
+import KSImage004 from '../components/images/KSImage004';
+import KSImage005 from '../components/images/KSImage005';
+import KSImage007 from '../components/images/KSImage007';
+import KSImage008 from '../components/images/KSImage008';
+import KSImage009 from '../components/images/KSImage009';
+import KSImage010 from '../components/images/KSImage010';
+import KSImage011 from '../components/images/KSImage011';
+import KSImage012 from '../components/images/KSImage012';
+import KSImage013 from '../components/images/KSImage013';
+import KSImage014 from '../components/images/KSImage014';
+import KSImage015 from '../components/images/KSImage015';
+import KSImage016 from '../components/images/KSImage016';
+import KSImage017 from '../components/images/KSImage017';
+import KSImage018 from '../components/images/KSImage018';
+import KSImage019 from '../components/images/KSImage019';
+import KSImage020 from '../components/images/KSImage020';
+import KSImage021 from '../components/images/KSImage021';
+import KSImage022 from '../components/images/KSImage022';
+import KSImage023 from '../components/images/KSImage023';
+import KSImage024 from '../components/images/KSImage024';
+import KSImage025 from '../components/images/KSImage025';
+
+const OurStory = () => {
+  return (
+    <Layout>
+      <SEO title="Our Story" keywords={[`wedding`, `engagement story`]} />
+      <TwoColumns>
+        <div>
+          <p>
+            Kyle and Shelly met on the dating app Coffee Meets Bagel. Kyle made her laugh with a
+            cute joke…and they seemed to hit it off. So they met at a local Starbucks and the rest
+            is history.
+          </p>
+
+          <p>
+            Kyle and Shelly love to travel. Hike and camp. They have hiked all over Arizona, Utah,
+            Idaho and California visiting many national parks.
+          </p>
+
+          <p>
+            On Kyle and Shelly’s hike up one of the tallest summits in the world, Mount Kilimanjaro,
+            Shelly jokingly said if they can stand sharing a tent after 7 days of hiking without
+            showers, they should just get married already ;&#41; Little did she know! On summit day,
+            Kyle proposed just before day- break at around 18,500 feet elevation. Shelly said yes of
+            course! There is video proof – but honestly you mostly just hear the wicked wind
+            whipping around and then an “Oh, babe” and more wind….and given it was just before
+            sunrise so you can’t see their Michelin-like coat- layered figures either &#40;which
+            Shelly is more than ok with&#41;.
+          </p>
+          <p>
+            We are so happy to share this special day with everyone. Thank you for taking the time
+            to celebrate with us!
+          </p>
+        </div>
+        <div>
+          <KSImage000 />
+          <KSImage001 />
+          <KSImage002 />
+          <KSImage003 />
+          <KSImage004 />
+          <KSImage005 />
+          <KSImage007 />
+          <KSImage008 />
+          <KSImage009 />
+          <KSImage010 />
+          <KSImage011 />
+          <KSImage012 />
+          <KSImage013 />
+          <KSImage014 />
+          <KSImage015 />
+          <KSImage016 />
+          <KSImage017 />
+          <KSImage018 />
+          <KSImage019 />
+          <KSImage020 />
+          <KSImage021 />
+          <KSImage022 />
+          <KSImage023 />
+          <KSImage024 />
+          <KSImage025 />
+        </div>
+      </TwoColumns>
+    </Layout>
+  );
+};
+
+export default OurStory;
---
+++ src/pages/travel.js
@@ -0,0 +1,193 @@
+import React from 'react';
+import SEO from '../components/seo';
+import Layout from '../components/layout';
+import { DarkAccent, LightAccent, DarkAccentLighter, TwoColumns } from '../shared/styledComponents';
+
+const travel = () => {
+  return (
+    <Layout>
+      <SEO title="Travel and Accommodations" keywords={[`wedding`, `engagement story`]} />
+      <h1>Guest Accommodations</h1>
+      <h2>Hotels:</h2>
+      <ul>
+        <li>
+          <a href="https://doubletree3.hilton.com/en/hotels/north-carolina/doubletree-by-hilton-hotel-asheville-biltmore-AVLBMDT/index.html">
+            DoubleTree by Hilton Asheville
+          </a>
+        </li>
+        <li>
+          <a href="https://www.residencesatbiltmore.com/">The Residences at Biltmore</a>
+        </li>
+        <li>
+          <a href="https://poshboutiquehotel.com/">Posh Boutique Hotel</a>
+        </li>
+        <li>
+          <a href="https://www.marriott.com/hotels/travel/avlri-residence-inn-asheville-biltmore/">
+            Residence Inn by Marriott Asheville Biltmore
+          </a>
+        </li>
+        <li>
+          <a href="https://hamptoninn3.hilton.com/en/hotels/north-carolina/hampton-inn-and-suites-asheville-biltmore-village-AVLBVHX/index.html">
+            Hampton Inn &amp; Suites Asheville Biltmore Village
+          </a>
+        </li>
+        <li>
+          <a href="https://www.ihg.com/holidayinn/hotels/us/en/asheville/avlnc/hoteldetail?cm_mmc=GoogleMaps-_-HI-_-US-_-AVLNC">
+            Holiday Inn &amp; Suites Asheville
+          </a>
+        </li>
+        <li>
+          <a href="https://www.choicehotels.com/north-carolina/asheville/clarion-hotels/nc598">
+            Clarion Inn Biltmore Village
+          </a>
+        </li>
+        <li>
+          <a href="https://brookstonelodgeasheville.com/">Brookstone Lodge</a>
+        </li>
+        <li>
+          <a href="https://www.marriott.com/hotels/travel/avlar-courtyard-asheville-airport/">
+            Courtyard by Marriott Asheville Airport Arden, NC
+          </a>
+        </li>
+      </ul>
+      <h2>VRBO/Air BnB Options</h2>
+      <TwoColumns>
+        <DarkAccent>
+          <h3>VRBO</h3>
+          <p>Here is a general search link for all the VRBO&apos;s in the area.</p>
+          <p>
+            <a href="https://www.vrbo.com/search/keywords:asheville-nc-usa/arrival:2020-06-19/departure:2020-06-21?adultsCount=2&petIncluded=false">
+              VBRO Search Page
+            </a>
+          </p>
+        </DarkAccent>
+        <LightAccent>
+          <h3>AirBnB</h3>
+          <p>Here is a link that will bring you to the search page.</p>
+          <p>
+            <a href="https://www.airbnb.com/s/Asheville--NC--United-States/homes?adults=2&children=0&checkin=2020-06-19&checkout=2020-06-21&source=mc_search_bar&click_referer=t%3ASEE_ALL%7Csid%3A752c12c6-c9c1-429b-88ec-06ff2b95bda9%7Cst%3ALANDING_PAGE_MARQUEE&title_type=NONE&refinement_paths%5B%5D=%2Fhomes">
+              AirBnB
+            </a>
+          </p>
+        </LightAccent>
+      </TwoColumns>
+      <h3>VRBO&apos;s Recommened to us</h3>
+      <ul>
+        <li>
+          <a href="http://thesinclairavl.com/">The Sinclair AVL</a>: 3 Residences at 59 Broadway
+        </li>
+        <li>
+          <a href="https://www.vrbo.com/218070">55.5 Broadway Downtown Loft</a>
+        </li>
+        <li>The Loft at 35 Patton - 2 BD/BA</li>
+        <li>
+          VRBO # 467351 - The Barn at Mountain Meadows
+          <li>
+            <a href="thebarn.viewatmountainmeadows.com">thebarn.viewatmountainmeadows.com</a>
+          </li>
+        </li>
+        <li>
+          VRBO# 800857 - <a href="https://www.vrbo.com/800857">The Bamboo House</a>
+        </li>
+      </ul>
+      <h3>AirBnB&apos;s Recommended to us</h3>
+      <ul>
+        <li>
+          <a href="https://www.airbnb.com/users/32054468/listings">Downton Annie Apartments</a>:
+          Four 2 bedroom , one bath private apts at 32 N. Ann Street
+        </li>
+        <li>
+          <a href="https://www.vrbo.com/1706853">Breathtaking Mountain Hideaway</a> – Blackberry
+          Nest – Remote yet Convenient
+          <li>About 45 minutes from wedding venu</li>
+        </li>
+        <li>
+          <a href="https://www.airbnb.com/users/181480147/listings">
+            80 LEX 4-beautiful apartments
+          </a>
+        </li>
+        <li>
+          <a href="https://www.airbnb.com/users/176979085/listings?user_id=176979085&s=50">
+            Hiawassee e-202 &amp; 4 Rugby Way
+          </a>
+        </li>
+      </ul>
+      <h3>P.O.P Rentals</h3>
+      <p>
+        <strong>Sarah Nie</strong>
+      </p>
+      <address>P.O.P. Rentals and Consulting, Inc. 828.337.3362 popRENTALS.com</address>
+
+      <LightAccent>
+        <p>Tiny Homes – these are about 30 minutes away from wedding venue</p>
+        <p>
+          <a href="https://www.aconybellnc.com/vacation-rentals">
+            https://www.aconybellnc.com/vacation-rentals
+          </a>
+        </p>
+      </LightAccent>
+      <DarkAccentLighter>
+        <h3>Large Group Rentals</h3>
+        <ul>
+          <li>
+            TREETOPS: 45 Audubon Dr, Asheville (Woodfin)
+            <a href="https://www.vrbo.com/3715906ha">https://www.vrbo.com/3715906ha</a>
+          </li>
+          <li>
+            RIVER HOUSE: 1060 Old Marshall Hwy, Asheville
+            <a href="https://www.vrbo.com/3872295ha">https://www.vrbo.com/3872295ha</a>
+          </li>
+          <li>
+            THE GABLES: 145 Holcombe Branch Rd, Weaverville
+            <a href="https://www.vrbo.com/4077437ha">https://www.vrbo.com/4077437ha</a>
+          </li>
+          <li>
+            Donny Brook: Beautiful 3 bedroom
+            <a href="https://www.vrbo.com/736000">https://www.vrbo.com/736000</a>
+          </li>
+        </ul>
+        <p>Modern Farmhouse on 140+ Acres of Conserved Land</p>
+        <p>Alexander, NC. Farm stay. 6 guests.</p>
+      </DarkAccentLighter>
+
+      <h2>Travel and Transportation</h2>
+
+      <p>Free Parking Passes for wedding venue during ceremony/reception</p>
+
+      <h3>Asheville Regional Airport</h3>
+      <address>
+        61 Terminal Dr. #1 <br /> Fletcher, NC 28732
+      </address>
+      <ul>
+        <li>This is the closest option – about 20 minutes from wedding venue</li>
+        <li>
+          <a href="https://www.google.com/flights?hl=en#flt=/m/0s15v.AVL.2020-06-19*AVL./m/0s15v.2020-06-21;c:USD;e:1;sd:1;t:f">
+            Google Flights Price Tracking
+          </a>
+        </li>
+        <li>
+          Easy Uber/Lyft from airport (around $20 – 30 depending on time of day/weather) to wedding
+          venue and downtown area
+        </li>
+        <li>
+          Does have Car Rental Options at airport (Budget, Dollar, Enterprise, Hertz, Thrifty)
+        </li>
+      </ul>
+
+      <h3>Greenville-Spartanburg International Airport</h3>
+      <address>2000 GSP Dr, Greer, SC 29651</address>
+      <ul>
+        <li>
+          <a href="https://www.google.com/flights?hl=en#flt=/m/0s15v.PGV.2020-06-19*PGV./m/0s15v.2020-06-21;c:USD;e:1;sd:1;t:f">
+            Google Flights Price Tracking
+          </a>
+        </li>
+        <li>About 1 hour and 20 minute drive to wedding venue</li>
+        <li>Likely not able to Uber/Lyft or Taxi due to length of drive</li>
+        <li>Does have Car Rental Options &#40;Avis, Hertz, Enterprise&#41;</li>
+      </ul>
+    </Layout>
+  );
+};
+
+export default travel;
